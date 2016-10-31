d76b6779569992d52db375a3a56ec9c865fa315b
nataivanova <natalyas.ivanova@gmail.com>
2016-10-31 16:03:17 +0200

    Adding cancel input fallback; bugged

--- js/script.js
+++ js/script.js
@@ -2,15 +2,28 @@
 var names = new Array();
 
 for (var i = 1; i<=5; i++) {
-    var name = prompt("Додайте ім'я:");
-    names.push(name);
-    console.log("Додано користувача " + i + ": " + name);
+    var name = prompt('Enter the ' + ', numbers only:', '0');
+    console.log(name);
+    console.log(typeof name);
+    if (name != null) {
+        console.log(name != null);
+        names.push(name);
+        console.log("Додано користувача " + i + ": " + name);
+    }
+    else {
+        console.log("Перервано користувачем.");
+        break;
+    }
 }
 
 var currentUser = prompt("Введіть ім'я користувача:");
 
 for (var j = 0; j < names.length; j++) {
     if (names[j] == currentUser) {
-        alert(currentUser + ", ви успішно зайшли в систему!");
+        alert("Вітаємо, " + currentUser + ", ви успішно зайшли в систему!");
+    }
+    else {
+        alert("Ім'я користувача не знайдено.");
+        break;
     }
 }

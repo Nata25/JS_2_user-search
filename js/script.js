1113f6594c5c2625214da5e59154e020bbce0336
nataivanova <natalyas.ivanova@gmail.com>
2016-10-31 16:22:20 +0200

    Implement whole prog with functions

--- js/script.js
+++ js/script.js
@@ -1,31 +1,41 @@
-insertNames();
+
+var array = insertNames();
+
+if (array != undefined) {
+    searchUser(array);
+}
+
 
 function insertNames() {
     var names = new Array();
 
     for (var i = 1; i<=5; i++) {
-        var name = prompt("Додайте користувача:", '0');
+        var name = prompt("Додайте користувача:");
         if (name != null) {
             names.push(name);
             console.log("Додано користувача " + i + ": " + name);
         }
         else {
             console.log("Перервано користувачем.");
-            break;
+            return;
         }
     }
+    return names;
 }
 
-//
-//
-// var currentUser = prompt("Введіть ім'я користувача:");
-//
-// for (var j = 0; j < names.length; j++) {
-//     if (names[j] == currentUser) {
-//         alert("Вітаємо, " + currentUser + ", ви успішно зайшли в систему!");
-//     }
-//     else {
-//         alert("Ім'я користувача не знайдено.");
-//         break;
-//     }
-// }
+function searchUser(names) {
+
+    // catch empty string
+    do {
+        var currentUser = prompt("Введіть ім'я користувача:");
+    } while (currentUser.length == 0);
+
+    for (var j = 0; j < names.length; j++) {
+        if (names[j] == currentUser) {
+            alert("Вітаємо, " + currentUser + ", ви успішно зайшли в систему!");
+            return;
+        }
+    }
+    alert("Ім'я користувача не знайдено.");
+    return;
+}

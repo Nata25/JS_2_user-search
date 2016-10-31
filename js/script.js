c93019f122e7ef71adbcefc129f44535e4013e65
nataivanova <natalyas.ivanova@gmail.com>
2016-10-31 16:08:40 +0200

    Validation of input works fine if prompt is inside function

--- js/script.js
+++ js/script.js
@@ -1,29 +1,31 @@
+insertNames();
 
-var names = new Array();
+function insertNames() {
+    var names = new Array();
 
-for (var i = 1; i<=5; i++) {
-    var name = prompt('Enter the ' + ', numbers only:', '0');
-    console.log(name);
-    console.log(typeof name);
-    if (name != null) {
-        console.log(name != null);
-        names.push(name);
-        console.log("Додано користувача " + i + ": " + name);
-    }
-    else {
-        console.log("Перервано користувачем.");
-        break;
-    }
-}
-
-var currentUser = prompt("Введіть ім'я користувача:");
-
-for (var j = 0; j < names.length; j++) {
-    if (names[j] == currentUser) {
-        alert("Вітаємо, " + currentUser + ", ви успішно зайшли в систему!");
-    }
-    else {
-        alert("Ім'я користувача не знайдено.");
-        break;
+    for (var i = 1; i<=5; i++) {
+        var name = prompt("Додайте користувача:", '0');
+        if (name != null) {
+            names.push(name);
+            console.log("Додано користувача " + i + ": " + name);
+        }
+        else {
+            console.log("Перервано користувачем.");
+            break;
+        }
     }
 }
+
+//
+//
+// var currentUser = prompt("Введіть ім'я користувача:");
+//
+// for (var j = 0; j < names.length; j++) {
+//     if (names[j] == currentUser) {
+//         alert("Вітаємо, " + currentUser + ", ви успішно зайшли в систему!");
+//     }
+//     else {
+//         alert("Ім'я користувача не знайдено.");
+//         break;
+//     }
+// }
